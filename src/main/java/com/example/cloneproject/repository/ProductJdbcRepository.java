90f82e5ae47ffc0ce81e0895a835467e1d7237d7
JeongYongCheol <dydcjf611@gmail.com>
2023-02-23 15:59:12 +0900

    add function delete, insert to ProductJdbcRepository + create Product Service

--- src/main/java/com/example/cloneproject/repository/ProductJdbcRepository.java
+++ src/main/java/com/example/cloneproject/repository/ProductJdbcRepository.java
@@ -33,7 +33,15 @@ public class ProductJdbcRepository implements ProductRepository {
 
     @Override
     public Product update(Product product) {
-        return null;
+        var update = jdbcTemplate.update(
+                "UPDATE products SET product_name = :productName, category = :category, price = :price, updated_at = :updatedAt" +
+                        "WHERE product_id = UUID_TO_BIN(:productId)",
+                toParamMap(product)
+        );
+        if (update != 1) {
+            throw new RuntimeException("Nothing was updated");
+        }
+        return product;
     }
 
     @Override
@@ -72,7 +80,7 @@ public class ProductJdbcRepository implements ProductRepository {
 
     @Override
     public void deleteAll() {
-
+        jdbcTemplate.update("DELETE FROM products", Collections.emptyMap());
     }
 
     private final RowMapper<Product> productRowMapper = (resultSet, i) -> {
---
+++ src/main/java/com/example/cloneproject/service/DefaultProductService.java
@@ -0,0 +1,37 @@
+package com.example.cloneproject.service;
+
+import com.example.cloneproject.model.Category;
+import com.example.cloneproject.model.Product;
+import com.example.cloneproject.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+public class DefaultProductService implements ProductService {
+
+    private final ProductRepository productRepository;
+    @Override
+    public List<Product> getProductsByCategory(Category category) {
+        return productRepository.findByCategory(category);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product createProduct(String productName, Category category, long price) {
+        var product = new Product(UUID.randomUUID(), productName, category, price);
+        return productRepository.insert(product);
+    }
+
+    @Override
+    public Product createProduct(String productName, Category category, long price, String description) {
+        var product = new Product(UUID.randomUUID(), productName, category, price, description, LocalDateTime.now(), LocalDateTime.now());
+        return productRepository.insert(product);
+    }
+}
